04269a38bea997bb5062a9f72d60d7332d9b3237
Krasimir Kostadinov <krasimir.kostadinov@scalefocus.com>
2019-08-11 15:02:27 +0300

    Adding the links to the main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import AboutPage from './AboutPage';
+import Primal from './Primal';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+          </a>
+        </header>
+        <div>
+              <Route exact path="/" component={Primal} />
+              <Route exact path="/about" component={AboutPage} />
+          </div>
+      </div>
+    </Router>
   );
 }
 
